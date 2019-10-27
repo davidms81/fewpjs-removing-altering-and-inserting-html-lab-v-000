7497f54913c4fadc647d4f8dfef31e69aa97fc06
David Schoenholtz <dmschoenholtz@gmail.com>
2019-10-27 14:38:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ document.querySelector('main#main').remove();
 let newHeader = document.createElement('h1')
 newHeader.id = 'victory'
 document.body.appendChild(newHeader)
+newHeader.innerHTML = 'David is the champion'
